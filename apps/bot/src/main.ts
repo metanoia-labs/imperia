c8cce58a3b07c74e35eac149fe91b92c9b455bed
elizielx <yehezkieldio35@gmail.com>
2024-02-20 09:13:07 +0800

    feat(bot): add initial bot files

--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import { SapphireClient } from "@sapphire/framework";
+import { GatewayIntentBits } from "discord.js";
+
+const client = new SapphireClient({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    baseUserDirectory: __dirname,
+    loadMessageCommandListeners: true,
+});
+
+client.login(process.env.DISCORD_TOKEN);
