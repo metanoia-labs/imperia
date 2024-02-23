5320d6ceef7afeb675330bebe637dba465cb2661
elizielx <yehezkieldio35@gmail.com>
2024-02-23 08:04:30 +0800

    fix(bot): properly remove system cmds if not dev and qol update on things

--- apps/bot/src/commands/informational/help.ts
+++ apps/bot/src/commands/informational/help.ts
@@ -1,7 +1,13 @@
 import { CommandField, EmbedBuilder, ImperiaCommand, SelectMenuOptions, checkDeveloper } from "@imperia/discord-bot";
 import { MessageBuilder, PaginatedMessage } from "@sapphire/discord.js-utilities";
 import { Command, CommandStore, RegisterBehavior } from "@sapphire/framework";
-import { Collection, ComponentType, SlashCommandBuilder, chatInputApplicationCommandMention } from "discord.js";
+import {
+    Collection,
+    ComponentType,
+    SlashCommandBuilder,
+    chatInputApplicationCommandMention,
+    hyperlink,
+} from "discord.js";
 
 export class HelpCommand extends ImperiaCommand {
     public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
@@ -24,6 +30,10 @@ export class HelpCommand extends ImperiaCommand {
     }
 
     public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        /**
+         * ! Here be dragons.
+         */
+
         await interaction.deferReply();
 
         const commands: CommandStore = this.container.stores.get("commands");
@@ -32,11 +42,33 @@ export class HelpCommand extends ImperiaCommand {
 
         const paginate: PaginatedMessage = new PaginatedMessage();
 
+        const invite = hyperlink(
+            "Invite to Server",
+            "https://discord.com/api/oauth2/authorize?client_id=911590809873301514&permissions=0&scope=applications.commands%20bot"
+        );
+        const website = hyperlink("Explore the Website", "https://imperia-bot.vercel.app/");
+        const support = hyperlink("Join Support Server", "https://discord.gg/KfhgHw7pvn");
+
         paginate.addPageBuilder((builder: MessageBuilder) => {
-            const embed = new EmbedBuilder().setTitle("Help!").setDescription("To be added.");
+            const embed = new EmbedBuilder()
+                .setAuthor({
+                    name: `${this.container.client.user.username}`,
+                    iconURL: this.container.client.user.displayAvatarURL(),
+                })
+                .setDescription(
+                    `Here, you can find a list of available commands and their descriptions. Please use the select menu below to navigate through the available command categories.\n\nInterested about Imperia? Use ${this.getCommandMention(
+                        "about"
+                    )} to find out more!`
+                )
+                .addFields({
+                    name: "— Useful Links",
+                    value: `${invite}\n${website}\n${support}`,
+                });
 
             return builder.setEmbeds([embed]);
         });
+
+        categories.splice(categories.indexOf("system"), !checkDeveloper(interaction.user.id) ? 1 : 0);
 
         for (const category of categories) {
             if (category === "primary") continue;
@@ -48,13 +80,9 @@ export class HelpCommand extends ImperiaCommand {
             const fields: ImperiaCommand[] = categoryCommands.map((cmd: ImperiaCommand) => cmd);
 
             const commandFields: CommandField[] = fields.map((cmd: ImperiaCommand) => {
-                const commandId: string = this.container.applicationCommandRegistries.acquire(cmd.name).globalCommandId;
-                let command;
+                const command = this.getCommandMention(cmd.name);
 
-                if (!commandId) command = `/${cmd.name}`;
-                else command = chatInputApplicationCommandMention(cmd.name, commandId);
-
-                return { name: command, value: `${cmd.description}`, inline: true };
+                return { name: command, value: cmd.description, inline: true } as CommandField;
             });
 
             paginate.addPageBuilder((builder: MessageBuilder) => {
@@ -69,15 +97,26 @@ export class HelpCommand extends ImperiaCommand {
         const selectMenuOptions: SelectMenuOptions[] = [];
 
         selectMenuOptions.push(
-            ...categories.map((category) => ({
-                label: category.charAt(0).toUpperCase() + category.slice(1),
-                description:
-                    category === "primary"
-                        ? "View the initial page."
-                        : `View ${category.charAt(0).toUpperCase() + category.slice(1)}-related commands.`,
-                value: category,
-            }))
+            ...categories
+                .map((category) => {
+                    if (category === "system" && !checkDeveloper(interaction.user.id)) {
+                        return null;
+                    }
+
+                    return {
+                        label: category.charAt(0).toUpperCase() + category.slice(1),
+                        description:
+                            category === "primary"
+                                ? "View the initial page."
+                                : `View ${category.charAt(0).toUpperCase() + category.slice(1)}-related commands.`,
+                        value: category,
+                    };
+                })
+                .filter(Boolean)
         );
+
+        this.container.logger.info(`Select menu options: ${JSON.stringify(selectMenuOptions)}`);
+        this.container.logger.info(`Categories: ${JSON.stringify(categories)}`);
 
         paginate.setActions([
             {
@@ -94,5 +133,14 @@ export class HelpCommand extends ImperiaCommand {
         ]);
 
         return paginate.run(interaction);
+    }
+
+    private getCommandMention(commandName: string): string | `</${string}:${string}>` {
+        const command = this.container.applicationCommandRegistries.acquire(commandName);
+        const commandId = command.globalChatInputCommandIds.values().next().value;
+
+        if (!commandId) return `/${commandName}`;
+
+        return chatInputApplicationCommandMention(command.commandName, commandId);
     }
 }
