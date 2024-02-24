c5c0328951c3404eb3ace891c37944ba4dafc457
elizielx <yehezkieldio35@gmail.com>
2024-02-24 19:31:48 +0800

    feat(bot): add a random waifu command

---
+++ apps/bot/src/commands/informational/random-waifu.ts
@@ -0,0 +1,36 @@
+import { EmbedBuilder, ImperiaCommand } from "@imperia/discord-bot";
+import { RegisterBehavior } from "@sapphire/framework";
+import { SlashCommandBuilder } from "discord.js";
+
+export class RandomWaifuCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            name: "random-waifu",
+            description: "Retrive a random waifu image from the internet",
+            requiredClientPermissions: ["SendMessages"],
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry) {
+        const command = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+
+        void registry.registerChatInputCommand(command, {
+            behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+            guildIds: [],
+            idHints: [],
+        });
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        return interaction.reply({
+            embeds: [new EmbedBuilder().setDescription(`Here's your waifu! :3`).setImage(await this.getWaifu())],
+        });
+    }
+
+    public async getWaifu() {
+        const response = await fetch("https://api.waifu.pics/sfw/waifu");
+        const data = await response.json();
+        return data.url;
+    }
+}
