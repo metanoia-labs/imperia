a27de03ff40ad2091673ed6bb4c21df3626cf6af
elizielx <yehezkieldio35@gmail.com>
2024-02-25 11:00:29 +0800

    feat(BankCommand): add skeleton files for a banking system

---
+++ apps/bot/src/commands/finance/bank.ts
@@ -0,0 +1,91 @@
+import { ImperiaCommand } from "@imperia/discord-bot";
+import { RegisterBehavior } from "@sapphire/framework";
+import { SlashCommandBuilder } from "discord.js";
+
+export class BankCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            name: "bank",
+            description: "Manage your bank account.",
+            subcommands: [
+                {
+                    name: "create",
+                    chatInputRun: "chatInputRunCreate",
+                },
+                {
+                    name: "balance",
+                    chatInputRun: "chatInputRunBalance",
+                },
+                {
+                    name: "deposit",
+                    chatInputRun: "chatInputRunDeposit",
+                },
+                {
+                    name: "withdraw",
+                    chatInputRun: "chatInputRunWithdraw",
+                },
+                {
+                    name: "transfer",
+                    chatInputRun: "chatInputRunTransfer",
+                },
+            ],
+            requiredClientPermissions: ["SendMessages"],
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry): void {
+        const command = new SlashCommandBuilder()
+            .setName(this.name)
+            .setDescription(this.description)
+            .addSubcommand((subcommand) => subcommand.setName("create").setDescription("Create a bank account."))
+            .addSubcommand((subcommand) =>
+                subcommand.setName("balance").setDescription("View your bank account balance.")
+            )
+            .addSubcommand((subcommand) =>
+                subcommand.setName("deposit").setDescription("Deposit money into your bank account.")
+            )
+            .addSubcommand((subcommand) =>
+                subcommand.setName("withdraw").setDescription("Withdraw money from your bank account.")
+            )
+            .addSubcommand((subcommand) =>
+                subcommand.setName("transfer").setDescription("Transfer money between bank accounts.")
+            );
+
+        void registry.registerChatInputCommand(command, {
+            behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+            guildIds: [],
+            idHints: [],
+        });
+    }
+
+    public async chatInputRunCreate(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        return interaction.reply({
+            content: "This command is not yet implemented.",
+        });
+    }
+
+    public async chatInputRunBalance(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        return interaction.reply({
+            content: "This command is not yet implemented.",
+        });
+    }
+
+    public async chatInputRunDeposit(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        return interaction.reply({
+            content: "This command is not yet implemented.",
+        });
+    }
+
+    public async chatInputRunWithdraw(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        return interaction.reply({
+            content: "This command is not yet implemented.",
+        });
+    }
+
+    public async chatInputRunTransfer(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        return interaction.reply({
+            content: "This command is not yet implemented.",
+        });
+    }
+}
